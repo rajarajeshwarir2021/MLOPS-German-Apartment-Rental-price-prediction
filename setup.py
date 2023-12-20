ebfdf3473018f17ed0628e56488b2a64125e04b7
rajar <rajarajeshwarir2021@gmail.com>
2023-12-20 11:41:19 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is an MLOPS based project for predicting German Apartment rental prices based on various demographic factors and preferential amenities.',
+    author='raji',
+    license='MIT',
+)
